3c976a4a8be3d5502a3c5d66e2c44f9ed9e00ea0
three-question-marks <misha20142@gmail.com>
2025-10-24 21:40:07 +0300

    feat: add 7-Zip and Google Chrome installers

--- wipe.ps1
+++ wipe.ps1
@@ -14,14 +14,24 @@ $instance = Get-CimInstance -Namespace $namespaceName -ClassName $className -Fil
 $session.InvokeMethod($namespaceName, $instance, $methodName, $params)
 '@
 $unattend_xml_source = "https://raw.githubusercontent.com/three-question-marks/wipe/refs/heads/main/unattend.xml"
-$anydesk_source = "https://download.anydesk.com/AnyDesk.exe"
 
 $null = New-Item -Path "C:\Recovery\AutoApply\CustomizationFiles\Drivers" -ItemType Directory -Force
 
 (New-Object System.Net.WebClient).DownloadFile($unattend_xml_source, "C:\Recovery\AutoApply\unattend.xml")
 
-Start-BitsTransfer -Source $anydesk_source -Destination "C:\Recovery\AutoApply\CustomizationFiles\AnyDesk.exe" `
+$source = "https://download.anydesk.com/AnyDesk.exe"
+Start-BitsTransfer -Source $source -Destination "C:\Recovery\AutoApply\CustomizationFiles\AnyDesk.exe" `
   -TransferPolicy "Always" -Description "Downloading Anydesk.exe..." -DisplayName "Anydesk" -ErrorAction "Continue"
+
+$release = Invoke-RestMethod 'https://api.github.com/repos/ip7z/7zip/releases/latest'
+$asset = $release.assets | Where-Object {$_.Name -match '^7z[0-9]+\.exe$'}
+$source = $asset.browser_download_url
+Start-BitsTransfer -Source $source -Destination "C:\Recovery\AutoApply\CustomizationFiles\7-Zip.exe" `
+  -TransferPolicy "Always" -Description "Downloading 7-Zip..." -DisplayName "7-Zip" -ErrorAction "Continue"
+
+$source = 'https://dl.google.com/chrome/install/latest/chrome_installer.exe'
+Start-BitsTransfer -Source $source -Destination "C:\Recovery\AutoApply\CustomizationFiles\Google Chrome.exe" `
+  -TransferPolicy "Always" -Description "Downloading Google Chrome online installer..." -DisplayName "Google Chrome" -ErrorAction "Continue"
 
 Write-Host -NoNewLine 'Press any key to wipe...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
